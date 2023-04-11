38feef6c8ee0658aa241f2f99043cd10f9ea852f
KayqueSilveira <48252382+KayqueSilveira@users.noreply.github.com>
2023-04-11 14:34:43 -0300

    Add files via upload

---
+++ sistemas/src/test/java/com/target/sistemas/SistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.target.sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
